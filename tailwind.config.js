31cacebb73b14bceb248f2b5d688f9d7f912e565
mayowa <smayowa176gmail.com>
2020-08-25 16:51:51 +0100

    creating an apartment website

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      container:{
+        center: true,
+        padding: '2rem'
+      },
+      colors:{
+        primary: '#ed5151',
+        secondary: {
+          100: '#22337d',
+          200: '#9c31df'
+        },
+        iconscol: '#a6a3ba'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
